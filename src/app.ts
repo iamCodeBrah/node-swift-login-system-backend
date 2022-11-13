690e8498764c58ce55f8873efe5cfd82e6f3f3cd
iamCodeBrah <letscodebrah@gmail.com>
2022-11-13 06:56:41 -0800

    Lesson 1

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Application, Router, Request, Response, NextFunction } from 'express';
+const app: Application = express();
+const cookieParser = require('cookie-parser');
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+   res.status(200).json({ success: 'Hello Server'});
+});
+
+module.exports = app;
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import http from 'http';
+const app = require('./app');
+
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on port ${port}`));
