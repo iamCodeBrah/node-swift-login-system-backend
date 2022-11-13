065584ca3393a29c0694d927a104f311a2cea826
iamCodeBrah <letscodebrah@gmail.com>
2022-11-13 07:39:42 -0800

    Lesson 2 + Lesson 3

---
+++ src/helpers/mysql_pools.ts
@@ -0,0 +1,14 @@
+import mysql from 'mysql';
+require('dotenv').config();
+
+const db_config = {
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DATABASE,
+    socketPath: process.env.SQL_SOCKET_PATH,
+};
+
+const pool = mysql.createPool(db_config);
+
+module.exports = pool;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import http from 'http';
 const app = require('./app');
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
